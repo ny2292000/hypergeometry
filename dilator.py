9e6210beb75d7d3f1b729d708667122cd35eb53b
ny2292000 <ny2292000@yahoo.com>
2016-03-07 12:42:05 -0500

    dilator is added

--- dilator.py
+++ dilator.py
@@ -1,7 +1,8 @@
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
-
+import pandas as pd
+from itertools import cycle
 
 
 
@@ -40,29 +41,74 @@ import numpy as np
 # These are the thicknesses along the radial direction (Fourth Dimensional direction perpendicular to our
 # 3D Universe (LightSpeed Expanding Hypersperical Universe).
 # The total 4D volume should be identical.
-hp=1e-6
-he=hp*1837
 
-protonCoeff = (2 / 3, 2 / 3, -1 / 3,hp)
-electronCoeff = (0,-2/3,-1/3,he)
-positronCoeff = (0,2/3,1/3,-he)
-antiprotonCoeff = (-2 / 3, -2 / 3, 1 / 3,-hp)
+def swap_cols(arr, frm, to):
+    arr[:,[frm, to]] = arr[:,[to, frm]]
+    return arr
+
+def swap_rows(arr, frm, to):
+    arr[[frm, to], :] = arr[[to, frm], :]
+    return arr
+
+def getSpin(axis=None):
+    spin = np.identity(4)
+    if axis is None:
+        return spin
+    spin=swap_cols(spin, axis,3)
+    return spin
+
+def getDilatorSequence(particle=0, axis=0, spin='half'):
+    hp = 1e-9
+    he = hp * 1837
+    ident = np.identity(4)
+    rotate = getSpin(axis=axis)
+    rotationMatrix = [ident,rotate,ident,rotate]
+    # Particle Definition
+    protonCoeff = np.array([2/3, 2/3, -1/3,hp]).T
+    electronCoeff = np.array([0,-2/3,-1/3,he]).T
+    positronCoeff = np.array([0,2/3,1/3,-he]).T
+    antiprotonCoeff = np.array([-2/3, -2/3, 1/3,-hp]).T
+    if(spin=='half'):
+        listA = [protonCoeff, electronCoeff,antiprotonCoeff, positronCoeff,protonCoeff, electronCoeff,antiprotonCoeff, positronCoeff]
+    else:
+        listA=[protonCoeff,positronCoeff ,antiprotonCoeff, electronCoeff,protonCoeff,positronCoeff ,antiprotonCoeff,electronCoeff ]
+    listA = listA[particle:(particle+4)]
+    dilatorSequence = [np.dot(rotationMatrix[i],listA[i]) for i in np.arange(4)]
+    A = [x[0:3] for x in dilatorSequence]
+    return A
+
 
 # The actual amplitude of the metric distorsion is not know. Considering that it is very, very small
 # the metric displacement will be approximated by the sum of the 3D coefficients times the radial thickness
 # hp is unknown
+class dilator(object):
+        def __init__(self,particle,axis,spin,ax,position=[0,0]):
+            self.unit = {}
+            self.ax=ax
+            self.particle=particle
+            self.dilatorSeq=getDilatorSequence(particle=particle, axis=axis, spin=spin)
+            self.axis=axis
+            self.center = [position + [i*np.pi/2 ]for i in np.arange(5)]
+            self.spin=spin
+        def plotMe(self):
+            i=0
+            max_radius = 2.5 * np.pi
+            for t in self.dilatorSeq:
+                self.unit[i]=unit(t,self.ax,self.center[i]).plotMe(ax)
+                i=i+1
+            plt.show()
 
 # Radii corresponding to the coefficients:
 class unit(object):
-    def __init__(self,coeffs,center=(0,0,0)):
+    def __init__(self,coeffs,ax,center=(0,0)):
+        self.ax = ax
         self.coeffs=coeffs
+        self.charge=np.sum(coeffs)
+        print(self.charge)
         self.center=center
-        a=1
-        print(a)
-        self.rx, self.ry, self.rz = [(e+1.0)/a for e in coeffs]
-        print(self.rx,self.ry,self.rz)
-        print(coeffs)
-
+        # Scaling factor
+        a=2
+        self.rx, self.ry, self.rz = [(e/a+1.0) for e in coeffs]
         # Set of all spherical angles:
         self.u = np.linspace(0, 2 * np.pi, 100)
         self.v = np.linspace(0, np.pi, 100)
@@ -71,37 +117,39 @@ class unit(object):
         # (this is the equation of an ellipsoid):
         u=self.u
         v=self.v
-        x = self.rx * np.outer(np.cos(u), np.sin(v))+self.center[0]
-        y = self.ry * np.outer(np.sin(u), np.sin(v))+self.center[1]
-        z = self.rz * np.outer(np.ones_like(u), np.cos(v))+self.center[2]
+        x = (self.rx-1) * np.outer(np.cos(u), np.sin(v))+self.center[0]
+        y = (self.ry-1) * np.outer(np.sin(u), np.sin(v))+self.center[1]
+        z = (self.rz-1) * np.outer(np.ones_like(u), np.cos(v))+self.center[2]
         return x,y,z
-    def plotMe(self):
-        fig = plt.figure(figsize=plt.figaspect(1))
-        # Square figure
-        ax = fig.add_subplot(111, projection='3d')
+    def plotMe(self,ax):
         # Plot:
         x,y,z = self.surface()
-        ax.plot_surface(x, y, z,  rstride=4, cstride=4, color='b')
-        # Adjustment of the axes, so that they all have the same span:
-        max_radius = max(self.rx, self.ry, self.rz)
-        max_radius = max(max_radius,self.center[0], self.center[1], self.center[2])
-        for axis in 'xyz':
-            getattr(ax, 'set_{}lim'.format(axis))((-max_radius, max_radius))
-        plt.show()
+        color='g'
+        if(self.charge<0):
+            color='r'
+        self.ax.plot_surface(x, y, z,  rstride=4, cstride=4, color=color)
 
 if(__name__=='__main__'):
-    vaccumCoeff = (0, 0, 0)
-    vaccum = dilator(vaccumCoeff)
-    vaccum.plotMe()
+    # for spin in ['half', 'minus-half']:
+    #     print('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx Spin=  ',spin)
+    #     for particle in np.arange(4):
+    #         print('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx  Particle=', particle)
+    #         for axis in np.arange(3):
+    #             print('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx  Axis=', axis)
+    #             A = getDilatorSequence(particle=particle, axis=axis, spin=spin)
+    #             print(' spin = ', spin, ' particle = ', particle, ' axis = ', axis)
+    #             for t in A:
+    #                 print(t)
+    #             print('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
+
+    fig = plt.figure(figsize=plt.figaspect(1))
+    # Square figure
+    ax = fig.add_subplot(111, projection='3d')
+    getattr(ax, 'set_{}lim'.format('x'))((-np.pi / 4, np.pi / 4))
+    getattr(ax, 'set_{}lim'.format('y'))((-np.pi / 4, np.pi / 4))
+    getattr(ax, 'set_{}lim'.format('z'))((-np.pi / 4, 2.25 * np.pi))
+    proton = dilator(particle=0,axis=0,ax=ax,spin='half',position=[0,0])
+    proton.plotMe()
     a=1
-    protonCoeff = (2 / 3, 2 / 3, -1 / 3)
-    proton1 = dilator(protonCoeff)
-    proton1.plotMe()
-    proton2 = dilator(protonCoeff,(0,0,2*np.pi))
-    proton2.plotMe()
-    electronCoeff = (0, -2 / 3, -1 / 3)
-    electron = dilator(electronCoeff)
-    electron.plotMe()
-    a=1
-    electron.plotMe(np.pi/4.0)
-    a=1
+
+
